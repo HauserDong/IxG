dcbcf444fef50030494c71dc589b4ac68723f243
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-10-13 19:43:44 -0400

    simplified test file

--- examples/insatxgcs/bfs_test.cpp
+++ examples/insatxgcs/bfs_test.cpp
@@ -32,86 +32,35 @@
  * \date 10/11/23
 */
 
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <common/insatxgcs/utils.hpp>
+#include <common/insatxgcs/gcsbfs.hpp>
 
-class Graph {
-public:
-  Graph() {}
-
-  void addEdge(int u, int v) {
-    adjacency[u].push_back(v);
-    adjacency[v].push_back(u);  // For an undirected graph
+auto printPath = [](int goal, const std::vector<int>& v) {
+  std::cout << "goal: " << goal << std::endl;
+  std::cout << "path: ";
+  for (int num : v) {
+    std::cout << num << " -> ";
   }
-
-  std::unordered_map<int, std::vector<int>> bfs_with_paths(int start) {
-    std::queue<int> q;
-    std::unordered_map<int, bool> visited;
-    std::unordered_map<int, std::vector<int>> paths;
-
-    for (const auto& kv : adjacency) {
-      int node = kv.first;
-      visited[node] = false;
-      paths[node] = std::vector<int>();
-    }
-
-    q.push(start);
-    visited[start] = true;
-
-    while (!q.empty()) {
-      int current = q.front();
-      q.pop();
-
-      for (const int neighbor : adjacency[current]) {
-        if (!visited[neighbor]) {
-          visited[neighbor] = true;
-          q.push(neighbor);
-
-          paths[neighbor] = paths[current];
-          paths[neighbor].push_back(current);
-        }
-      }
-    }
-
-    return paths;
-  }
-
-private:
-  std::unordered_map<int, std::vector<int>> adjacency;
+  std::cout << std::endl;
 };
 
 int main() {
-  Graph graph;
-
   auto edges_bw_regions = utils::DeserializeEdges("../examples/insatxgcs/resources/maze2d/maze_edges.csv");
-  for (auto& edge : *edges_bw_regions) {
-    graph.addEdge(edge.first, edge.second);
-  }
+  ixg::GCSBFS graph(*edges_bw_regions);
 
   int start_node = 0;
 
-  std::unordered_map<int, std::vector<int>> paths = graph.bfs_with_paths(start_node);
+  auto start_time = std::chrono::high_resolution_clock::now();
+  std::unordered_map<int, std::vector<int>> paths = graph.BFSWithPaths(start_node);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  std::cout << "BFS completed in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count()/1e9 << "s" << std::endl;
 
   std::cout << "Paths from node " << start_node << " to other nodes:" << std::endl;
   for (const auto& kv : paths) {
-    int node = kv.first;
-    std::vector<int> path = kv.second;
 
-    std::cout << "To node " << node << ": ";
-    if (node == start_node) {
-      std::cout << "Start Node" << std::endl;
-    } else if (!path.empty()) {
-      std::cout << start_node;
-      for (int i = path.size() - 1; i >= 0; --i) {
-        std::cout << " -> " << path[i];
-      }
-      std::cout << " -> " << node << std::endl;
-    } else {
-      std::cout << "Not reachable" << std::endl;
-    }
+    printPath(kv.first, kv.second);
+//    printPath(0, paths[0]);
+
+    break;
   }
 
   return 0;
